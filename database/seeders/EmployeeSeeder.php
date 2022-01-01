93340d43c8c77e3d25bc0f3e3c950e222d910955
saralimbu2017 <iamsebi7@gmail.com>
2022-01-02 08:45:38 +1100

    Configuring database values in database seeder

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -14,5 +14,12 @@ class EmployeeSeeder extends Seeder
     public function run()
     {
         //
+        Employee::create([
+            'name' => 'John Doe',
+            'email' => 'doejohn@gmail.com',
+            'phone' => '1234567890',
+            'department' => 'hr',
+            'dob' => '2010-01-03',
+        ]);
     }
 }
